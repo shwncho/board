13b9300669326c0ecfa7410d2a8c7214be0277d9
shwncho <shwncho4@gmail.com>
2024-09-22 16:49:56 +0900

    feat: design api for like

---
+++ src/main/kotlin/com/example/board/repository/LikeRepository.kt
@@ -0,0 +1,6 @@
+package com.example.board.repository
+
+import com.example.board.domain.Like
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface LikeRepository : JpaRepository<Like, Long>

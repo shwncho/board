996b0db255f182f603b2a13c264a1d7b174083e1
shwncho <shwncho4@gmail.com>
2024-09-22 17:14:46 +0900

    feat: add countlike functions for likes

--- src/main/kotlin/com/example/board/controller/dto/PostDetailResponse.kt
+++ src/main/kotlin/com/example/board/controller/dto/PostDetailResponse.kt
@@ -19,5 +19,6 @@ fun PostDetailResponseDto.toResponse() = PostDetailResponse(
     content = content,
     createdBy = createdBy,
     createdAt = createdAt,
-    comments = comments.map { it.toResponse() }
+    comments = comments.map { it.toResponse() },
+    likeCount = likeCount
 )
